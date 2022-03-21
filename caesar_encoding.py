433f15fabee42f54b74bdcd47fcc1e17236f0ed2
ryuvi <vicenters10@gmail.com>
2022-03-21 07:54:34 -0300

    Adjusted caesar cipher

--- caesar_encoding.py
+++ caesar_encoding.py
@@ -1,71 +1,38 @@
+# INIT PROJECT CAESAR
+
 import random
 
 message_sent = open('sent.txt', 'a')
 message_received = open('received.txt', 'a')
-lAlphabet = [
-    ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", 's', "t", "u", "v", "w",
-     "x", "y", "z"],
-    ["b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", 's', "t", "u", "v", "w", "x",
-     "y", "z", "a"],
-    ["c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", 's', "t", "u", "v", "w", "x", "y",
-     "z", "a", "b"],
-    ["d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", 's', "t", "u", "v", "w", "x", "y", "z",
-     "a", "b", "c"],
-    ["e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", 's', "t", "u", "v", "w", "x", "y", "z", "a",
-     "b", "c", "d"],
-    ["f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", 's', "t", "u", "v", "w", "x", "y", "z", "a", "b",
-     "c", "d", "e"],
-    ["g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", 's', "t", "u", "v", "w", "x", "y", "z", "a", "b", "c",
-     "d", "e", "f"],
-    ["h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", 's', "t", "u", "v", "w", "x", "y", "z", "a", "b", "c", "d",
-     "e", "f", "g"],
-    ["i", "j", "k", "l", "m", "n", "o", "p", "q", "r", 's', "t", "u", "v", "w", "x", "y", "z", "a", "b", "c", "d", "e",
-     "f", "g", "h"],
-    ["j", "k", "l", "m", "n", "o", "p", "q", "r", 's', "t", "u", "v", "w", "x", "y", "z", "a", "b", "c", "d", "e", "f",
-     "g", "h", "i"],
-    ["k", "l", "m", "n", "o", "p", "q", "r", 's', "t", "u", "v", "w", "x", "y", "z", "a", "b", "c", "d", "e", "f", "g",
-     "h", "i", "j"],
-    ["l", "m", "n", "o", "p", "q", "r", 's', "t", "u", "v", "w", "x", "y", "z", "a", "b", "c", "d", "e", "f", "g", "h",
-     "i", "j", "k"],
-    ["m", "n", "o", "p", "q", "r", 's', "t", "u", "v", "w", "x", "y", "z", "a", "b", "c", "d", "e", "f", "g", "h", "i",
-     "j", "k", "l"],
-    ["n", "o", "p", "q", "r", 's', "t", "u", "v", "w", "x", "y", "z", "a", "b", "c", "d", "e", "f", "g", "h", "i", "j",
-     "k", "l", "m"],
-    ["o", "p", "q", "r", 's', "t", "u", "v", "w", "x", "y", "z", "a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k",
-     "l", "m", "n"],
-    ["p", "q", "r", 's', "t", "u", "v", "w", "x", "y", "z", "a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l",
-     "m", "n", "o"],
-    ["q", "r", 's', "t", "u", "v", "w", "x", "y", "z", "a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m",
-     "n", "o", "p"],
-    ["r", 's', "t", "u", "v", "w", "x", "y", "z", "a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n",
-     "o", "p", "q"],
-    ['s', "t", "u", "v", "w", "x", "y", "z", "a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o",
-     "p", "q", "r"],
-    ["t", "u", "v", "w", "x", "y", "z", "a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p",
-     "q", "r", "s"],
-    ["u", "v", "w", "x", "y", "z", "a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q",
-     "r", "s", "t"],
-    ["v", "w", "x", "y", "z", "a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r",
-     "s", "t", "u"],
-    ["w", "x", "y", "z", "a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s",
-     "t", "u", "v"],
-    ["x", "y", "z", "a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t",
-     "u", "v", "w"],
-    ["y", "z", "a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u",
-     "v", "w", "x"],
-    ["z", "a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v",
-     "w", "x", "y"],
-]
+lAlphabet = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", 's', "t", "u",
+             "v", "w", "x", "y", "z"]
+
 nCipherLevel = random.randint(0, 25)
 
 
+def set_cipher_alphabet(n_cipher_level):
+    l_temp_alphabet = []
+    n_temp_alphabet_index = n_cipher_level
+    while n_temp_alphabet_index < len(lAlphabet):
+        l_temp_alphabet.append(lAlphabet[n_temp_alphabet_index])
+        n_temp_alphabet_index += 1
+
+    n_temp_alphabet_index = 0
+    while n_temp_alphabet_index < n_cipher_level:
+        l_temp_alphabet.append(lAlphabet[n_temp_alphabet_index])
+        n_temp_alphabet_index += 1
+
+    return l_temp_alphabet
+
+
 def init():
+    cipher_alphabet = set_cipher_alphabet(nCipherLevel)
     s_option = input('Want to [1]encode or [2]decode? ')
 
     if s_option in ('1', 'encode'):
         print('Insert the message:')
         s_message = input('> ')
-        encode(s_message.lower())
+        encode(s_message.lower(), cipher_alphabet)
 
     elif s_option in ('2', 'decode'):
         print('Insert the message:')
@@ -76,32 +43,32 @@ def init():
         init()
 
 
-def grab_letter_encode(p_s_letter):
+def grab_letter_encode(p_s_letter, ciphered_alphabet):
     n_index = 0
 
-    for s_char in lAlphabet[0]:
+    for s_char in lAlphabet:
         if p_s_letter == s_char:
-            return lAlphabet[nCipherLevel][n_index]
+            return ciphered_alphabet[n_index]
 
         n_index += 1
 
 
-def grab_letter_decode(p_s_letter, cipher_level):
+def grab_letter_decode(p_s_letter, ciphered_alphabet):
     n_index = 0
 
-    for s_char in lAlphabet[cipher_level]:
+    for s_char in ciphered_alphabet:
         if p_s_letter == s_char:
-            return lAlphabet[0][n_index]
+            return lAlphabet[n_index]
 
         n_index += 1
 
 
-def encode(p_s_message):
+def encode(p_s_message, ciphered_alphabet):
     s_result = ''
 
     for s_char in p_s_message:
         if s_char != ' ':
-            s_result += grab_letter_encode(s_char)
+            s_result += grab_letter_encode(s_char, ciphered_alphabet)
 
         else:
             s_result += s_char
@@ -115,10 +82,11 @@ def decode(p_s_message):
     p_l_message = p_s_message.split(' ')
     n_message_cipher_level = int(p_l_message[0])
     n_index = 1
+    l_cipher_alphabet = set_cipher_alphabet(n_message_cipher_level)
 
     while n_index < len(p_l_message):
         for s_char in p_l_message[n_index]:
-            s_result += grab_letter_decode(s_char, n_message_cipher_level)
+            s_result += grab_letter_decode(s_char, l_cipher_alphabet)
 
         if n_index != len(p_l_message)-1:
             s_result += ' '
